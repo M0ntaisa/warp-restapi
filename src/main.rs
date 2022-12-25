0956d1ae9881fc5827051dc8d4faf1e7dd28bfb1
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2022-12-25 20:37:08 +0800

    add index.html

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use warp::Filter;
 
+const WEB_FOLDER: &str ="web-folder/";
+
 #[tokio::main]
 async fn main() {
-    let hello_world = warp::path::end()
-        .and(warp::get())
-        .map(|| "Hellow World From Root!");
-
+    // APIs
     let hi = warp::path("hi")
         .and(warp::get())
         .map(|| "hi doumo");
+    let apis = hi;
 
-    let routes = hello_world.or(hi);
+    // Static Content
+    let content = warp::fs::dir(WEB_FOLDER);
+    let root = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(format!("{}/index.html", WEB_FOLDER)));
+    let static_site = content.or(root);
+    
+    let routes = apis.or(static_site);
 
     println!("Start web-server");
     warp::serve(routes).run(([127,0,0,1], 8080)).await;
